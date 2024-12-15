74326695c3a4942bd71c04e96a581a0994148e81
Sellik16 <ku.sell02@gmail.com>
2024-12-15 12:45:57 +0100

    kalendarz przechowuje zdarzenia, ale niestety jedno na każdy dzień

---
+++ ProjektPlaner/Models/CalendarDayViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjektPlaner.Models
+{
+    public class CalendarDayViewModel
+    {
+        public DateTime Date { get; set; } // Data tego dnia w kalendarzu
+        public string? EventName { get; set; } // Nazwa wydarzenia, które odbywa się tego dnia
+    }
+}
--- ProjektPlaner/Models/CalendarElement.cs
+++ ProjektPlaner/Models/CalendarElement.cs
@@ -10,24 +10,30 @@ namespace ProjektPlaner.Models
         [Display(Name = "Name")]
         [StringLength(100, ErrorMessage = "The {0} must be at max {1} characters long.", MinimumLength = 2)]
         public string Name { get; set; }
+
         [Display(Name = "Description")]
         [StringLength(100, ErrorMessage = "The {0} must be at max {1} characters long.")]
         public string? Description { get; set; }
-        [Display(Name = "Start")]
-        public DateTime StartDate { get; set; }
-        [Display(Name = "End")]
-        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Start Date")]
+        [DataType(DataType.DateTime)]
+        public DateTime StartDate { get; set; } // Data rozpoczęcia wydarzenia
+
+        [Display(Name = "End Date")]
+        [DataType(DataType.DateTime)]
+        public DateTime EndDate { get; set; } // Data zakończenia wydarzenia
+
         [Display(Name = "Location")]
         [StringLength(100, ErrorMessage = "The {0} must be at max {1} characters long.")]
         public string? Location { get; set; }
 
-        public string? UserId { get; set; }
-            
+        public string? UserId { get; set; } // Id użytkownika, który stworzył wydarzenie
+
         public IdentityUser? User { get; set; }
 
         [Display(Name = "Group")]
         public int? GroupId { get; set; } = null!;
-        public CalendarGroup? Group { get; set; }
 
+        public CalendarGroup? Group { get; set; } // Powiązana grupa wydarzenia
     }
 }
